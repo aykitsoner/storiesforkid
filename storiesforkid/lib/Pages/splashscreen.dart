51452f482af6d95dce3f2478e149f17e0de7ffd7
aykitsoner <aykitsoner@outlook.com>
2023-01-11 20:09:30 +0300

    splashscreen design

--- storiesforkid/lib/Pages/splashscreen.dart
+++ storiesforkid/lib/Pages/splashscreen.dart
@@ -17,10 +17,27 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 
   Widget mainWidget() {
-    return Center(
-        child:
-            // Load a Lottie file from a remote url
-            Lottie.network(
-                'https://assets8.lottiefiles.com/packages/lf20_rzp5gdfi.json'));
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomCenter,
+          colors: [
+            Colors.deepPurple.withOpacity(0.8),
+            Colors.purple.withOpacity(0.8),
+            Colors.blue.withOpacity(0.8),
+            Colors.green.withOpacity(0.8),
+            Colors.yellow.withOpacity(0.8),
+            Colors.orange.withOpacity(0.8),
+            Colors.red.withOpacity(0.8),
+          ],
+        ),
+      ),
+      child: Center(
+          child:
+              // Load a Lottie file from a remote url
+              Lottie.network(
+                  'https://assets8.lottiefiles.com/packages/lf20_rzp5gdfi.json')),
+    );
   }
 }
